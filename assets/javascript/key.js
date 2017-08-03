73dcb51b340581274276138f697ab83c96a11e34
Helmut Cardenas <aleksey89col@gmail.com>
2017-08-03 01:31:25 -0400

    rough finish of contac us

--- assets/javascript/key.js
+++ assets/javascript/key.js
@@ -6,3 +6,6 @@ var config = {
     storageBucket: "funwithrealstate.appspot.com",
     messagingSenderId: "73499968045"
   };
+
+var SID =  "ACd6d01b00a7dc16a5accf109cf7636fe4";
+var Key = "0dcba94c1c9f96b702e886d03cbce898";
--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -599,13 +599,13 @@ $("#btn-logOut").on("click", function(e) {
   
 });
 
-// init controller
-var controller = new ScrollMagic.Controller();
+// // init controller
+// var controller = new ScrollMagic.Controller();
 
-// create a scene
-new ScrollMagic.Scene({
-        duration: 100,    // the scene should last for a scroll distance of 100px
-        offset: 50        // start this scene after scrolling for 50px
-    })
-    .setPin("#results-panel") // pins the element for the the scene's duration
-    .addTo(controller); // assign the scene to the controller
+// // create a scene
+// new ScrollMagic.Scene({
+//         duration: 100,    // the scene should last for a scroll distance of 100px
+//         offset: 50        // start this scene after scrolling for 50px
+//     })
+//     .setPin("#results-panel") // pins the element for the the scene's duration
+//     .addTo(controller); // assign the scene to the controller
